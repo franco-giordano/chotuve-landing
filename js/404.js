3dd526a82d9c00cb66d262172e4c5132a2b15bd9
franco-giordano <francogior98@gmail.com>
2020-07-24 20:25:18 -0300

    try scheme button

--- js/404.js
+++ js/404.js
@@ -4,4 +4,15 @@ const url = window.location.href
 if (url) {
     const paths = url.split('/');
     console.log(paths);
+
+    const btn = document.getElementById('open');
+
+    if (paths[3] == 'videos' && paths[4]) {
+        btn.hidden = false;
+        btn.href = "chotuve://videos/" + paths[4];
+    }
+    else if (paths[3] == 'users' && paths[4]) {
+        btn.hidden = false;
+        btn.href = "chotuve://users/" + paths[4];
+    }
 }
